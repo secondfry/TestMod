345522a28e35abeee52d530378f7f9927810a0d9
Rustam Gubaydullin <secondfry@gmail.com>
2013-09-23 02:16:28 +0400

    Required fixes - implementing IEntityAdditionalSpawnData

--- ru/secondfry/TestMod/entities/EntityRocket.java
+++ ru/secondfry/TestMod/entities/EntityRocket.java
@@ -1,5 +1,8 @@
 package ru.secondfry.TestMod.entities;
 
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import cpw.mods.fml.common.registry.IEntityAdditionalSpawnData;
 import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
@@ -9,14 +12,14 @@ import net.minecraft.world.World;
  * User: Rustam Second_Fry Gubaydullin
  * Date: 21.09.13 7:41
  */
-public class EntityRocket extends Entity {
+public class EntityRocket extends Entity implements IEntityAdditionalSpawnData {
 
 	private int type = -1;
 	private int start = -1;
 
 	public EntityRocket(World world) {
 		super(world);
-		motionY = 0.5;
+		motionY = 0.1;
 	}
 
 	@Override
@@ -56,5 +59,15 @@ public class EntityRocket extends Entity {
 	@Override
 	protected void writeEntityToNBT(NBTTagCompound compound) {
 		compound.setByte("Type", (byte) type);
+	}
+
+	@Override
+	public void writeSpawnData(ByteArrayDataOutput data) {
+		data.writeInt(type);
+	}
+
+	@Override
+	public void readSpawnData(ByteArrayDataInput data) {
+		type = data.readInt();
 	}
 }
